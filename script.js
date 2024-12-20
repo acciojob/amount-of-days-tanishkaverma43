b7a54c51f168ff1afb8b661c59a9901433768c8b
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2024-12-20 12:30:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    // Check if the year is a leap year
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return 366; // Leap year has 366 days
+    } else {
+        return 365; // Non-leap year has 365 days
+    }
+}
+console.log(daysOfAYear(2020));
